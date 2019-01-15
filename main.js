7399d664276aa39d309ac3bb2051539af698aaeb
Douglas Bamber <douglasbamber@gmail.com>
2019-01-15 13:25:07 +1300

    FIX impliment correct main.js file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 (function() {
   var xmlhttp = new XMLHttpRequest();
-  var url = 'https://wt-douglasbamber-gmail_com-0.sandbox.auth0-extend.com/gjgardner';
+  var url = 'https://wt-douglasbamber-gmail_com-0.sandbox.auth0-extend.com/plans';
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -17,7 +17,18 @@
     var out = "";
     var i;
     for(i = 0; i < arr.length; i++) {
-      out += '<div>' + arr[i].title + '</div><br>';
+      out += [
+        '<a href="' + arr[i].link + '" target="_blank" >',
+        '<img src="' + arr[i].thumbnailImage + '" alt="' + arr[i].title + '" width="100">',
+        '<br>',
+        arr[i].title,
+        '<br>',
+        '<small>' + arr[i].collectionTitle + '</small>',
+        '<br>',
+        'Floor Area' + arr[i].floorArea + ' m<sup>2</sup>',
+        '</a>',
+        '<br>'
+      ].join('');
     }
     document.getElementById("plans").innerHTML = out;
   }
